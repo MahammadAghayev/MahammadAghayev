634518f21b04b463aedc21741dabce5bb8820126
mehemmed agayev <mehemmedagayev@MacBook-Air-mehemmed.local>
2023-12-05 00:33:54 +0300

    .

--- app/product/admin.py
+++ app/product/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Product, ProductItem, ProductType, Color, Size, Brand
 # Register your models here.
 
+class InlineSizeAdmin(admin.TabularInline):
+    model= Size
+    extra = 1
+
+class InlineColorAdmin(admin.TabularInline):
+    model= Color
+    extra = 1
+
+@admin.register(ProductType)      
+class PoductTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = (InlineSizeAdmin, InlineColorAdmin)
+
+
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductItem)
+admin.site.register(Brand)
+# admin.site.register(Size)
--- app/product/views.py
+++ app/product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.object.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'product/list.html', context)
